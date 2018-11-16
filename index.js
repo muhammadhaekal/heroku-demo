9a9b46d189cbfaa8af721de0d1accb0ea5e03506
Muhammad Haekal <93haekal@gmail.com>
2018-11-16 07:40:05 +0700

    Add CORS

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
 const PORT = process.env.PORT || 3000
+const cors = require("cors")
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
 
 let todoList = [
     {
